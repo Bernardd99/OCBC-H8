75b13edb0ddbf150817c2c112024567c1e55ded4
Bernardd99 <bnardd99@gmail.com>
2021-10-27 22:16:54 +0700

    Sesi 17

---
+++ Sesi 17/028_Bernard.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 2;
+
+const aIncrement = a+1;
+const bDecrement = b-1;
+
+console.log("Sum: ", a+b)
+console.log("Difference: ", (a-b))
+console.log("Product: ", a*b)
+console.log("Quotient: ", a/b)
+console.log("Remainder: ", a%b)
+console.log("Value of num1 after increment: ", aIncrement)
+console.log("Value of num2 after decrement: ", bDecrement)
